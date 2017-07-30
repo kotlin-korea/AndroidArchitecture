50adb56ab65a309a0e11abaceed92cf8d15da706
pluu-serface <pluulove@gmail.com>
2017-07-30 12:15:49 +0900

    FirstOrNull 써보기

--- app/src/main/java/iammert/com/androidarchitecture/viewmodel/MovieViewModelFactory.kt
+++ app/src/main/java/iammert/com/androidarchitecture/viewmodel/MovieViewModelFactory.kt
@@ -26,21 +26,14 @@ constructor(private val creators: Map<Class<out ViewModel>, Provider<out ViewMod
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        var creator: Provider<out ViewModel>? = creators[modelClass]
-        creator?.let {
-            for ((key, value) in creators) {
-                if (modelClass.isAssignableFrom(key)) {
-                    creator = value
-                    break
-                }
-            }
+        val creator = creators[modelClass]
+                ?: creators.entries.firstOrNull { modelClass.isAssignableFrom(it.key) }?.value
+                ?: throw IllegalArgumentException("unknown model class " + modelClass)
+
+        try {
+            return creator.get() as T
+        } catch (e: Exception) {
+            throw RuntimeException(e)
         }
-        creator?.let {
-            return try {
-                it.get() as T
-            } catch (e: Exception) {
-                throw RuntimeException(e)
-            }
-        } ?: throw IllegalArgumentException("unknown model class " + modelClass)
     }
 }
